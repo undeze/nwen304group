b54f0a34d643bce91a8eba53afda23b7eeed38f6
Chris Underwood <undeze@hotmail.com>
2016-05-17 08:38:54 +1200

    modify index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT;
+var bodyParser = require('body-parser');
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL;
+var client = new pg.Client(connectionString);
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.static(__dirname + '/'));
 
-app.get('/', function(req,res){
-  res.render('index');
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.listen(port,function(){
+	console.log('Listening');
 });
