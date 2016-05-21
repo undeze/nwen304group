ee2faf71c55afa066abc03877441aa65475bac4c
Chris Underwood <undeze@hotmail.com>
2016-05-22 11:55:57 +1200

    passport-local passport.use LocalStrategy

--- index.js
+++ index.js
@@ -80,6 +80,21 @@ app.set('view engine', 'ejs');
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 
 
 app.get('/', function(req,res){
