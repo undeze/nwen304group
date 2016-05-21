8dd2884fc29f800605132a6a4ef4896747fd21be
Chris Underwood <undeze@hotmail.com>
2016-05-21 19:25:35 +1200

    mod index.ejs

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ app.post('/signup', urlencodedparser, function(req,res){
 			}
 			hash.update(password);
 			var encrypted = hash.digest('hex');
-			var query = client.query("insert into members values (7,'" + username + 
+			var query = client.query("insert into members values (8,'" + username + 
 				"','" + encrypted + "','" + email + "');", function(error, result){
 					done();
 					if(error){}
