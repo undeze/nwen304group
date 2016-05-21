a97b68bda14123475bef000c94c3e594ceb2c524
Chris Underwood <undeze@hotmail.com>
2016-05-21 19:22:18 +1200

    mod index.ejs

--- index.js
+++ index.js
@@ -166,8 +166,9 @@ app.post('/signup', urlencodedparser, function(req,res){
 					console.log('Could not connect to postgresql on signup',err);
 					return;
 			}
-			var encrypted = hash.update(password);
-			var query = client.query("insert into members values (6,'" + username + 
+			hash.update(password);
+			var encrypted = hash.digest('hex');
+			var query = client.query("insert into members values (7,'" + username + 
 				"','" + encrypted + "','" + email + "');", function(error, result){
 					done();
 					if(error){}
