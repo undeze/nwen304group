080345af8c151268cb0919c303384da230a92b74
Chris Underwood <undeze@hotmail.com>
2016-05-22 15:34:34 +1200

    post /login

--- index.js
+++ index.js
@@ -239,13 +239,13 @@ app.post('/login', urlencodedparser, function(req,res){
 		hash.update(password);
 		var encrypted = hash.digest('hex');
 		
-		if(encrypted == passwordHash){
+		if(password == passwordHash){
 			console.log('successful login');
 		} else {
 			console.log('unsuccessful login');
 		}
 			
-			
+			//e48443929f57ec4cb965d358a825849155b2828873c4ed2929fd711bc9f01347
 			
 			
 		});
