b7a405971bbbc9ff7c9ea20dd73a5244fba2801d
underwchri <undeze@hotmail.com>
2016-05-23 17:53:40 +1200

    auth

--- index.js
+++ index.js
@@ -141,20 +141,38 @@ app.get('/login',
 		res.render('pages/login');
 	});
 
-app.get('/loginnew', loginGet);
+app.post('/loginnew', loginPost);
 
-function loginGet(req, res){
-  	if(req.user){
-    	// already logged in
-    	res.redirect('/');
-  } else {
-    	// not logged in, show the login form, remember to pass the message
-    	// for displaying when error happens
-    	res.render('pages/login', { message: req.session.messages });
-    	// and then remember to clear the message
-    	req.session.messages = null;
-  }
-};
+function loginPost(req, res, next) {
+  // ask passport to authenticate
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      // if error happens
+      return next(err);
+    }
+    
+    if (!user) {
+      // if authentication fail, get the error message that we set
+      // from previous (info.message) step, assign it into to
+      // req.session and redirect to the login page again to display
+      req.session.messages = info.message;
+      return res.redirect('/login');
+    }
+
+    // if everything's OK
+    req.logIn(user, function(err) {
+      if (err) {
+        req.session.messages = "Error";
+        return next(err);
+      }
+
+      // set the message
+      req.session.messages = "Login successfully";
+      return res.redirect('/');
+    });
+    
+  })(req, res, next);
+}
 
 app.get('/login/facebook',
 	passport.authenticate('facebook'));
