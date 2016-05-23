f1aacf3b027444e07ffea379ba9af69021e7e514
Chris Underwood <undeze@hotmail.com>
2016-05-23 21:36:38 +1200

    local auth

--- index.js
+++ index.js
@@ -51,7 +51,8 @@ passport.use(new LocalStrategy({
 
 	function(username, password, done){
 
-
+		console.log('LocalStrategy');
+		
 		pg.connect(connectionString, function (err, client, done){
 			if(err){
 					console.log('Could not connect to postgresql on signup',err);
@@ -91,28 +92,6 @@ passport.use(new LocalStrategy({
 
 			});	
 		});	
-
-		/*
-			//Query database
-				
-				// if there are any errors, return the error before anything else
-            	if (err)
-                	return done(err);
-
-            	// if no user is found, return the message
-            	if (!user)
-                	return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-
-            	// if the user is found but the password is wrong
-            	if (!user.validPassword(password))
-                	return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-
-            	// all is well, return successful user
-            	return done(null, user);
-			}*/
-		  
-		
-
 	}
 ));
 
