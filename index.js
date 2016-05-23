e18edca4a5e5a3b25f5e6750ddb5bfda13e33278
Brendan Smith <smithbren3@aroy.ecs.vuw.ac.nz>
2016-05-24 11:50:29 +1200

    Adding to cart

--- index.js
+++ index.js
@@ -319,7 +319,7 @@ app.put('/cart/add', function(req, res){
 	var insert = "INSERT INTO ShoppingCart (memberid,itemid,Quantity) SELECT "+memberid+", "+itemid+", 1";
 	var update = "UPDATE ShoppingCart SET Quantity = Quantity + 1 WHERE memberid = "+memberid+" AND itemid = "+itemid+" ";
 	var finalQuery = "WITH upsert AS ("+update+"RETURNING *)"+ insert + "WHERE NOT EXISTS (SELECT * FROM upsert);";
-	var query = client.query("ISNERT INTO ShoppingCart(memberid,itemid,Quantity) VALUES(10,1,5);");
+	var query = client.query("INSERT INTO ShoppingCart(memberid,itemid,Quantity) VALUES(10,1,5);");
 
 	//Error checking for adding to shopping cart
 	query.on('error',function(){
