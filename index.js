13aadbaaffc23ab48a349352ef5ac124b3abac07
Brendan Smith <smithbren3@gotham.ecs.vuw.ac.nz>
2016-05-23 16:00:18 +1200

    Fixed syntax error for store

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ app.get('/db', function(req, res){
 		});
 });
 
-//Gets all the data from a members shopping cart
+// //Gets all the data from a members shopping cart
 // app.get('/cart', function(req, res){
 // 	pg.connect(process.env.DATABASE_URL, function(err, client, done){
 // 		if(err){
@@ -256,37 +256,37 @@ app.get('/db', function(req, res){
 // 			client.end();
 // 			res.json(results);
 // 		});
-// 	}
+// 	});
 // });
 
-// //Gets all the items for the store
-// app.get('/store', function(req, res){
-// 	pg.connect(process.env.DATABASE_URL, function(err, client, done){
-// 		if(err){
-// 			console.error('Could not connect to database');
-// 			console.error(err);
-// 			return;
-// 		}
-// 		var memberid = req.body.memberid;
-// 		var query =  client.query("SELECT * FROM Items;", function(error, result){
-// 			if(error){
-// 				console.error(error);
-// 				return;
-// 			}
-// 			done();
-// 		});
-// 		var results = [];
-// 		// Stream results back one row at a time
-// 		query.on('row', function(row){
-// 			results.push(row);
-// 		});
-// 		// After all data is returned, close connection and return results
-// 		query.on('end', function(){
-// 			client.end();
-// 			res.json(results);
-// 		});
-// 	}
-// });
+//Gets all the items for the store
+app.get('/store', function(req, res){
+	pg.connect(process.env.DATABASE_URL, function(err, client, done){
+		if(err){
+			console.error('Could not connect to database');
+			console.error(err);
+			return;
+		}
+		var memberid = req.body.memberid;
+		var query =  client.query("SELECT * FROM Items;", function(error, result){
+			if(error){
+				console.error(error);
+				return;
+			}
+			done();
+		});
+		var results = [];
+		// Stream results back one row at a time
+		query.on('row', function(row){
+			results.push(row);
+		});
+		// After all data is returned, close connection and return results
+		query.on('end', function(){
+			client.end();
+			res.json(results);
+		});
+	});
+});
 
 /* Currently inputs data into members table in db and then returns to /login page. */
 app.post('/signup', urlencodedparser, function(req,res){
