e90c2ca5cdadc8e9d26ecec68e9ffcda159ce33b
underwchri <undeze@hotmail.com>
2016-05-23 13:25:36 +1200

    LocalStrategy

--- index.js
+++ index.js
@@ -52,23 +52,23 @@ passport.use('local-signup',new LocalStrategy({
 			User.findOne({}, function(err,user){
 				
 				// if there are any errors, return the error before anything else
-            if (err)
-                return done(err);
+            	if (err)
+                	return done(err);
 
-            // if no user is found, return the message
-            if (!user)
-                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+            	// if no user is found, return the message
+            	if (!user)
+                	return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
 
-            // if the user is found but the password is wrong
-            if (!user.validPassword(password))
-                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+            	// if the user is found but the password is wrong
+            	if (!user.validPassword(password))
+                	return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
-            // all is well, return successful user
-            return done(null, user);
+            	// all is well, return successful user
+            	return done(null, user);
 
 
 			}
-			});
+		  );
 		});
 
 	}
