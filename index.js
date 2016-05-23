1fe26bfd367e539b2f8b7348de3232dcbe209caa
underwchri <undeze@hotmail.com>
2016-05-23 13:19:03 +1200

    LocalStrategy

--- index.js
+++ index.js
@@ -36,7 +36,43 @@ passport.use(new FacebookStrategy({
 		// allows for account linking and authentication with other identity
 		// providers.
 		return cb(null, profile);
-	}));
+	}
+));
+
+// https://scotch.io/tutorials/easy-node-authentication-setup-and-local
+passport.use(new LocalStrategy({
+		usernameField: 'email',
+		passwordField: 'password',
+		passReqToCallback: true
+	},
+
+	function(req, email, passord, done){
+
+		process.nextTick(function(){
+			User.findOne({}, function(err,user){
+				
+				// if there are any errors, return the error before anything else
+            if (err)
+                return done(err);
+
+            // if no user is found, return the message
+            if (!user)
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+
+            // if the user is found but the password is wrong
+            if (!user.validPassword(password))
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
+            // all is well, return successful user
+            return done(null, user);
+
+
+			}
+			})
+		})
+
+	}
+));
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
