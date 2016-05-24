ab3ffa03bd31718dd1969b283c62436b3ebfe5aa
Brendan Smith <smithbren3@aroy.ecs.vuw.ac.nz>
2016-05-25 11:36:46 +1200

    Testing adding to cart

--- index.js
+++ index.js
@@ -357,14 +357,15 @@ app.put('/cart/add', function(req, res){
 	var insert = "INSERT INTO ShoppingCart (memberid,itemid,Quantity) SELECT "+memberid+", "+itemid+", 1";
 	var update = "UPDATE ShoppingCart SET Quantity = Quantity + 1 WHERE memberid = "+memberid+" AND itemid = "+itemid+" ";
 	var finalQuery = "WITH upsert AS ("+update+"RETURNING *)"+ insert + "WHERE NOT EXISTS (SELECT * FROM upsert);";
-	var query = client.query(finalQuery);
+	//var query = client.query(finalQuery);
+	var query = client.query("WITH upsert AS (UPDATE ShoppingCart SET Quantity = Quantity + 1 WHERE memberid = "+memberid+" AND itemid = "+itemid" RETURNING *) INSERT INTO ShoppingCart (memberid,itemid,Quantity) SELECT 8,1,1  WHERE NOT EXISTS (SELECT * FROM upsert);");
 	//var query = client.query("INSERT INTO ShoppingCart(memberid,itemid,Quantity) VALUES(10,1,5);");
 
 	//Error checking for adding to shopping cart
 	query.on('error',function(){
 		return response.status(500).send('Error updating shopping cart');
 	});
-	res.send("Item has been added to cart");
+	res.send("Item has been added to cart\n");
 });
 
 /* Currently inputs data into members table in db and then returns to /login page. */
