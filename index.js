3fc15a13cb641b6e3416f5c2d885a6ed9f564dbb
Chris Underwood <undeze@hotmail.com>
2016-05-28 12:33:05 +1200

    req.session.passport.user = nu

--- index.js
+++ index.js
@@ -197,11 +197,9 @@ function loginPost(req, res, next) {
       	// Set the message
       	req.session.messages = "Login successfully";
 
-      	//var nu = { displayName : 'fred'};
+      	
       	var nu = { displayName : username };
-      	//req.session.passport.user.displayName = "fred";// = { displayName: 'fred'};
-      	//req.session.user = req.session.passport.user.displayName;
-      	//req.session.user = nu;
+      	
       	req.session.passport.user = nu;
 
       	console.log('loginPost successful');
