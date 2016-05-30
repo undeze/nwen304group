e7ebb6792ae5a77dc640b4df6a830799dbcc1af9
Brendan Smith <smithbren3@reds.ecs.vuw.ac.nz>
2016-05-30 15:29:11 +1200

    Working on shopping cart linking with database

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -189,7 +189,7 @@ function getData(){
 //Redraws the shopping cart for the client
 function refreshList(data){
 	//Loop through all items in the cart database
-	alert(data);
+	alert(data[0].name);
 	for(items in data){
 		var itemName = data[items].name;
 		var price = data[items].price;
