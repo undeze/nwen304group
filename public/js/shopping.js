59f9e5843ce7f87f062ef6015d80aa729d422487
Brendan Smith <smithbren3@rubia.ecs.vuw.ac.nz>
2016-06-01 13:20:04 +1200

    Changed weather to wellington city

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -202,7 +202,8 @@ function refreshList(data){
 
 function getWeather(){
 	$.ajax({
-		url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%2015021762&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
+		//url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%2015021762&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
+		url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%2029344823&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
 		method: "GET",
 		dataType: "jsonp",
 		success: function(data){
