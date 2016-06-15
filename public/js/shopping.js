935a8aad2a71c4d491125bebd70e8b89270e3789
SwiftyNZ <smithbren3@the-pub.ecs.vuw.ac.nz>
2016-06-15 12:36:47 +1200

    Getting cart UI to work with database

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -17,41 +17,41 @@ $(document).ready(function() {
 		//Add item to cart database
 		addToCart(itemname);
 		
-		// if(include(Arrays,thisID))
-		// {
-		// 	var price 	 = $('#each-'+thisID).children(".shopp-price").find('em').html();
-		// 	var quantity = $('#each-'+thisID).children(".shopp-quantity").html();
-		// 	quantity = parseInt(quantity)+parseInt(1);
+		if(include(Arrays,thisID))
+		{
+			var price 	 = $('#each-'+thisID).children(".shopp-price").find('em').html();
+			var quantity = $('#each-'+thisID).children(".shopp-quantity").html();
+			quantity = parseInt(quantity)+parseInt(1);
 			
-		// 	var total = parseInt(itemprice)*parseInt(quantity);
+			var total = parseInt(itemprice)*parseInt(quantity);
 			
-		// 	$('#each-'+thisID).children(".shopp-price").find('em').html(total);
-		// 	$('#each-'+thisID).children(".shopp-quantity").html(quantity);
+			$('#each-'+thisID).children(".shopp-price").find('em').html(total);
+			$('#each-'+thisID).children(".shopp-quantity").html(quantity);
 			
-		// 	var prev_charges = $('.cart-total span').html();
-		// 	prev_charges = parseInt(prev_charges)-parseInt(price);
+			var prev_charges = $('.cart-total span').html();
+			prev_charges = parseInt(prev_charges)-parseInt(price);
 			
-		// 	prev_charges = parseInt(prev_charges)+parseInt(total);
-		// 	$('.cart-total span').html(totalPrice);
+			prev_charges = parseInt(prev_charges)+parseInt(total);
+			$('.cart-total span').html(totalPrice);
 			
-		// 	$('#total-hidden-charges').val(prev_charges);
-		// }
-		// else
-		// {
-		// 	Arrays.push(thisID);
+			$('#total-hidden-charges').val(prev_charges);
+		}
+		else
+		{
+			Arrays.push(thisID);
 			
-		// 	var prev_charges = $('.cart-total span').html();
-		// 	prev_charges = parseInt(prev_charges)+parseInt(itemprice);
+			var prev_charges = $('.cart-total span').html();
+			prev_charges = parseInt(prev_charges)+parseInt(itemprice);
 			
-		// 	$('.cart-total span').html(prev_charges);
-		// 	$('#total-hidden-charges').val(prev_charges);
+			$('.cart-total span').html(prev_charges);
+			$('#total-hidden-charges').val(prev_charges);
 			
-		// 	var Height = $('#cart_wrapper').height();
-		// 	$('#cart_wrapper').css({height:Height+parseInt(45)});
+			var Height = $('#cart_wrapper').height();
+			$('#cart_wrapper').css({height:Height+parseInt(45)});
 			
-		// 	$('#cart_wrapper .cart-info').append('<div class="shopp" id="each-'+thisID+'"><div class="label">'+itemname+'</div><div class="shopp-price"> $<em>'+itemprice+'</em></div><span class="shopp-quantity">1</span><img src="remove.png" class="remove" /><br class="all" /></div>');
-		// }
-		// $('.detail-view').slideUp('slow');
+			$('#cart_wrapper .cart-info').append('<div class="shopp" id="each-'+thisID+'"><div class="label">'+itemname+'</div><div class="shopp-price"> $<em>'+itemprice+'</em></div><span class="shopp-quantity">1</span><img src="remove.png" class="remove" /><br class="all" /></div>');
+		}
+		$('.detail-view').slideUp('slow');
 	});	
 	
 	$('.remove').livequery('click', function() {
