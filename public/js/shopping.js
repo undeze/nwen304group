c7812d6678b4d83cf4eb535a38d358a23fa29505
SwiftyNZ <smithbren3@bengal-tiger.ecs.vuw.ac.nz>
2016-06-22 14:45:41 +1200

    Store recommendations from weather

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -212,9 +212,11 @@ function getWeather(){
 			temp = ((temp - 32) * 5)/9;
 			temp = temp.toFixed(1);
 			var condition = data.query.results.channel.item.condition.text;
+			var conditionCode = data.query.results.channel.item.condition.code;
 			var desc = data.query.results.channel.item.description;
+			var response = getRecommendation(conditionCode,temp);
 			desc = desc.match(/"[^"]+"/g);
-			$('.footer').children('#weather').html("<b>Current Conditions:</b> <br />"+condition+"</b> <br />"+temp+" Degrees </b> <br /> <img src="+desc+"/>");
+			$('.footer').children('#weather').html("<b>Current Conditions:</b> <br />"+condition+"</b> <br />"+temp+" Degrees </b> <br /> <img src="+desc+"/> <br /> "+response);
 		},
 		error: function(){
 			alert('Error retreiving weather results');
@@ -251,6 +253,59 @@ function makePurchase(){
 		});
 };
 
-function getRecommendation(weather,temperature){
-	
+function getRecommendation(weatherCode,temperature){
+	var response = "";
+	if(weatherCode <= 4){
+		response = "The weather looks terrible out there, you may want some gumboots";
+	}
+	else-if(weatherCode <= 12){
+		response = "Stay nice and dry with a waterproof jacket";
+	}
+	else-if(weatherCode <= 18){
+		response = "Get some warm gloves to play out in the snow";
+	}
+	else-if(weatherCode <= 22){
+		response = "Could be hard to see out there, maybe look for some glasses";
+	}
+	else-if(weatherCode <= 24){
+		response = "It's looking really windy out there, check out some windproof jackets";
+	}
+	else-if(weatherCode <= 25){
+		response = "Stay warm with a nice warm jacket";
+	}
+	else-if(weatherCode <= 30){
+		if(temperature < 10){
+			response = "Cloudy and cold, check out some warm pants";
+		}
+		else{
+			response = "Cloudy but not cold, check out some whiskey";
+		}
+	}
+	else-if(weatherCode <= 34){
+		if(temperature > 20){
+			response = "It's looking beautiful out there, check out our range of singlets";
+		}
+		else-if(temperature > 15){
+			response = "It's looking nice out there, check out our range of t-shirts";
+		}
+		else{
+			response = "It's looking nice out there, not too warm though, stay protected with some sunnies";
+		}
+	}
+	else-if(weatherCode <= 35){
+		rsponse = "Rain, with chances of hail, maybe you should check out some weatherproof gear";
+	}
+	else-if(weatherCode <= 36){
+		response = "It sure is hot outside, check out some sunnies and singlets to stay cool";
+	}
+	else-if(weatherCode <= 40){
+		response = "It's an inside day today, enjoy it with some whiskey";
+	}
+	else-if(weatherCode <= 43){
+		response = "Go play in the snow with some warm gloves and boots";
+	}
+	else{
+		response = "Check out our whole range, there will be something everyone will love";
+	}
+	return response;
 };
