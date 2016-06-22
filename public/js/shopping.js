bd92e29597b2b7bead2e541c5a3b3ded676a723c
SwiftyNZ <smithbren3@bengal-tiger.ecs.vuw.ac.nz>
2016-06-22 13:25:30 +1200

    Shopping cart total pricing fix

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -194,6 +194,7 @@ function refreshList(data){
 		$('#cart_wrapper .cart-info').append('<div class="shopp" id="each-'+items+'"><div class="label">'+itemName+'</div><div class="shopp-price"> $<em>'+price+'</em></div><span class="shopp-quantity">'+quantity+'</span><img src="remove.png" class="remove" /><br class="all" /></div>');
 		$('#cart_wrapper').css({height:Height+parseInt(45)});
 	}
+	alert(totalPrice);
 	$('.cart-total span').html(totalPrice);
 };
 
