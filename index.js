64dcd8be1b07440e78a78a4a63eab2918196c143
SwiftyNZ <smithbren3@lazeeza.ecs.vuw.ac.nz>
2016-06-23 17:00:42 +1200

    Getting current logged in user

--- index.js
+++ index.js
@@ -475,7 +475,7 @@ app.post('/cart/delete', function(req, res){
 app.post('/cart/purchase', function(req, res){
 	pg.connect(process.env.DATABASE_URL, function(err, client, done){
 		var member = req.user.displayName;
-		var query = client.query("INSERT INTO purchases (price,datepurchased,itemname,colour,member) SELECT s.quantity*i.price AS price,NOW(),s.itemname,i.colour,s.member FROM shoppingcart s INNER JOIN items i ON s.itemname = i.name WHERE member = '"+member+"'; DELETE FROM shoppingcart WHERE member = '"+member"';");
+		var query = client.query("INSERT INTO purchases (price,datepurchased,itemname,colour,member) SELECT s.quantity*i.price AS price,NOW(),s.itemname,i.colour,s.member FROM shoppingcart s INNER JOIN items i ON s.itemname = i.name WHERE member = '"+member+"'; DELETE FROM shoppingcart WHERE member = '"+member+"';");
 
 		//Error checking for adding to purchases
 		query.on('error', function(){
