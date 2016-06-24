f9d37d9d73b33f91ef106a0189cc6204ad82b76d
SwiftyNZ <smithbren3@chocolate-days.ecs.vuw.ac.nz>
2016-06-24 17:03:13 +1200

    Connecting weather to geolocation

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -201,11 +201,6 @@ function refreshList(data, isFirstLoad){
 
 //Gets the weather data from yahoo to be displayed
 function getWeather(){
-	var trying = $('#city').innerHTML;
-	while (typeof trying == 'undefined'){
-		trying = $('#city').innerHTML;
-	}
-	console.log("City: "+trying);
 	$.ajax({
 		//https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Wellington%2C%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
 		url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%2029344823&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
